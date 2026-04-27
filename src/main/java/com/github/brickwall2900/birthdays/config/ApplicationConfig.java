317d3fb1d9fb38771a289ed3beef12a890b6537a
brickwall2900 <brickwall2900@gmail.com>
2026-04-27 15:09:43 +0800

    sonion

--- src/main/java/com/github/brickwall2900/birthdays/config/ApplicationConfig.java
+++ src/main/java/com/github/brickwall2900/birthdays/config/ApplicationConfig.java
@@ -2,6 +2,7 @@ package com.github.brickwall2900.birthdays.config;
 
 import java.time.LocalDate;
 
+// son ;-;
 public class ApplicationConfig {
     public boolean darkMode = false;
     public LocalDate lastAlive = LocalDate.now();
